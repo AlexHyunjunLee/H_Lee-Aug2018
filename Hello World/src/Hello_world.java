43857d6e91d7f13a2d97afe4e081f9db0243577d
APCS3rd <APCS3rd@MHS-Math2-33>
2018-08-31 14:52:07 -0700

    My Hello World works

---
+++ Hello World/src/Hello_world.java
@@ -0,0 +1,14 @@
+
+/**
+ * It prints out Hello_world.
+ * 
+ * @author Alex Lee 
+ * @version Aug 31, 2018
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Alex"); //Display the string
+    }
+}
