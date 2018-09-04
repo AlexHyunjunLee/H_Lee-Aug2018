22bc2ee9bdcc547364ae490f0824f07962338785
Alex Lee <Alex Lee@DESKTOP-VSMC0R8>
2018-09-03 22:03:42 -0700

    The pikachu prints out!

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,16 @@
+
+/**
+ * It prints out Pikachu.
+ * 
+ * @author Alex Lee 
+ * @version Sept. 3, 2018
+ */
+public class Pikachu {
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("     (\\__/)");
+		System.out.println("     (o^.^)");
+		System.out.println("   z(_(\")(\")");
+	}
+
+}
