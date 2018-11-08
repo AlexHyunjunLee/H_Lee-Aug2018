80f099ad7793f40e935e1a119134f72ebf5cdb1a
APCS3rd <APCS3rd@MHS-Math2-33>
2018-11-08 09:49:21 -0800

    Ole

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -14,8 +14,26 @@ public class LotsOfCopies {
 		System.out.println("x: " + num);
 		System.out.println("strMain: " + strMain);
 		System.out.println("arrMain: " + Arrays.toString(arrMain));
+		part2WithInts();
+		part2WithStrings();
 	}
 	public static void changeMe(int x, String str, int[] arr) {
-		
+		x = 229886;
+		str += str;
+		arr[4] = 100;
+	}
+	public static void part2WithInts() {
+		int a = 7;// a = startValue
+		int b = a;// b = a
+		a = 1;// a = newValue
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+	}
+	public static void part2WithStrings() {
+		String a = "Hi";// a = startValue
+		String b = a;// b = a
+		a = "Bye";// a = newValue
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
 	}
 }
